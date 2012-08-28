b1e64cee63759a0d7eff72803bd183e4c3b6ec99
Jed Parsons <jedp@me.com>
2012-08-27 20:24:35 -0700

    added config test

---
+++ idp/tests/test_config.js
@@ -0,0 +1,23 @@
+const vows = require('vows'),
+      assert = require('assert');
+
+var suite = vows.describe('config');
+
+suite.addBatch({
+  "The config": {
+    topic: function() {
+      var config = require("../lib/configuration");
+      this.callback(null, config);
+    },
+
+    'has a spiffy getter': function(config) {
+      assert(typeof config.get === 'function');
+    }
+  }
+});
+
+if (process.argv[1] === __filename) {
+  suite.run();
+} else {
+  suite.export(module);
+}
